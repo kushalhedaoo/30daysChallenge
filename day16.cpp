2302fecf89c98f6ab18d1f8fafaaca23a3ef7f5c
kushal07 <90027593+kushalhedaoo@users.noreply.github.com>
2023-08-20 23:54:09 +0530

    gcd

--- day16.cpp
+++ day16.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+using namespace std;
 
+int gcd(int a,int b){
+    while(b!=0){
+        int rem=a%b;
+        a=b;
+        b=rem;
+    }
+    return a;
+}
+
+int main()
+{
+    cout<<gcd(15,20);
+    return 0;
+}
